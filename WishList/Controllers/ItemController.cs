e1b8e65111785614e2b0c1de1c1ecbc39e3bf470
Mark Damen <40217274+mark-damen@users.noreply.github.com>
2022-04-01 23:41:40 +0100

    wrong

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
     public class ItemController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
+
+        [HttpPost]
+        public IActionResult Create(Item)
+        {
+            return;
+        }
+
     }
 }
