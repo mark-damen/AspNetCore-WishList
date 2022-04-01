cd0724eb4a6decaf006d5a098d98beef3880c87f
Mark Damen <40217274+mark-damen@users.noreply.github.com>
2022-04-01 22:57:54 +0100

    Updated

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items;
+        
+    }
+}
